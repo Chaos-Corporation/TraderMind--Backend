292c14665690a6e4709cc2b0cec06689e22838e9
Muhammad Sharjeel <sharjeelwaheed125@gmail.com>
2024-07-02 19:51:18 +0500

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+import uvicorn
+from app.api.api_router import api_router, auth_router
+from app.core.config import get_settings
+
+app = FastAPI(
+    title="Trader AI",
+    version="1.0.0",
+    description="https://github.com/Chaos-Corporation/TradingMind--Backend",
+    openapi_url="/openapi.json",
+    docs_url="/",
+)
+
+app.include_router(auth_router)
+app.include_router(api_router)
+
+# Sets all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        str(origin).rstrip("/")
+        for origin in get_settings().security.backend_cors_origins
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Guards against HTTP Host Header attacks
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=get_settings().security.allowed_hosts,
+)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
